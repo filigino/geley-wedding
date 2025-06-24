8a4ea229e50cc1f74022fff8f30ab99de3574848
Gino <gino.salayo@gmail.com>
2025-06-23 21:50:26 -0400

    routes, civil wedding

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import { SaveTheDate } from 'pages/SaveTheDate/SaveTheDate'
-
-const App = () => <SaveTheDate />
-
-export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import 'index.css'
+import reportWebVitals from 'reportWebVitals'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import { CivilWedding } from 'pages/CivilWedding/CivilWedding'
+import { SaveTheDate } from 'pages/SaveTheDate/SaveTheDate'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<SaveTheDate />} />
+        <Route path="/civil-wedding" element={<CivilWedding />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
