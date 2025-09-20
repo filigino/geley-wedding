ad26905c590c49ab95d066210e8c353d616939a7
Gino Salayo <gino.salayo@gmail.com>
2025-09-20 00:52:26 -0400

    wip

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
+
+import seeYouThere from 'assets/invitation/seeYouThere.png'
+import { Image } from 'components/Image/Image'
+import { BLACK, BLUE } from 'constants/colours'
+import { ADOBE_HANDWRITING_ERNIE } from 'constants/fonts'
+
+export const Footer = () => (
+  <Stack
+    sx={{ backgroundColor: BLUE, gap: '20px', px: '40px', overflow: 'visible' }}
+  >
+    <Image src={seeYouThere} sx={{ width: '67%' }} />
+    <Box alignSelf="flex-end">
+      <Typography
+        variant="h4"
+        sx={{
+          color: BLACK,
+          fontFamily: ADOBE_HANDWRITING_ERNIE,
+          ml: '36px',
+        }}
+      >
+        Love,
+      </Typography>
+      <Typography
+        variant="h4"
+        sx={{
+          color: BLACK,
+          fontFamily: ADOBE_HANDWRITING_ERNIE,
+        }}
+      >
+        Gino & Lesley
+      </Typography>
+    </Box>
+  </Stack>
+)
--- src/pages/Invitation/Invitation.tsx
+++ src/pages/Invitation/Invitation.tsx
@@ -5,6 +5,8 @@ import Stack from '@mui/material/Stack'
 
 import hero from 'assets/invitation/hero.png'
 import timeline from 'assets/invitation/timeline.png'
+import weddingCrew from 'assets/invitation/weddingCrew.png'
+import { Footer } from 'components/Footer/Footer'
 import { Image } from 'components/Image/Image'
 import { LogoDivider } from 'components/LogoDivider/LogoDivider'
 import { Rsvp } from 'components/Rsvp/Rsvp'
@@ -23,6 +25,9 @@ export const Invitation = () => (
       <WeddingDetails />
       <LogoDivider />
       <SpecialThanks />
+      <Image src={weddingCrew} sx={{ mt: '20px' }} />
+      <Rsvp />
+      <Footer />
     </Stack>
   </Container>
 )
