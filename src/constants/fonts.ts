1dc2f7f9016ab1e5cd4f763b3fd22493b0885c7f
Gino Salayo <gino.salayo@gmail.com>
2025-09-25 23:21:11 -0400

    font size

--- src/constants/fonts.ts
+++ src/constants/fonts.ts
@@ -2,7 +2,7 @@ export const ADOBE_HANDWRITING_ERNIE = 'adobe-handwriting-ernie, sans-serif'
 export const LATO = 'lato, sans-serif'
 export const LINOTYPE_DIDOT = 'linotype-didot, serif'
 
-export const FONT_SIZE_CONTENT = ['11px', '18px']
+export const FONT_SIZE_CONTENT = ['12px', '18px']
 export const FONT_SIZE_H4 = ['34px', '66px']
 export const FONT_SIZE_H5 = ['24px', '48px']
 export const FONT_SIZE_H6 = ['20px', '34px']
